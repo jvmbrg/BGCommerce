e3e28daa81b5307b9fd047c64b430df6b342ec0c
jvmbrg <jvitormbraga@gmail.com>
2025-12-17 21:17:08 -0300

    Criação da entidade User

---
+++ src/main/java/com/devbraga/bragacommerce/BgcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devbraga.bragacommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devbraga/bragacommerce/BgcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbraga.bragacommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
