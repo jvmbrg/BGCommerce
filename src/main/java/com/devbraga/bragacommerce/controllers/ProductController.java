ec4e687f79aeae9eba465b54c842bb3ccdbd8cf4
jvmbrg <jvitormbraga@gmail.com>
2026-01-06 15:45:49 -0300

    GET products paged

--- src/main/java/com/devbraga/bragacommerce/controllers/ProductController.java
+++ src/main/java/com/devbraga/bragacommerce/controllers/ProductController.java
@@ -3,12 +3,12 @@ package com.devbraga.bragacommerce.controllers;
 import com.devbraga.bragacommerce.dto.ProductDTO;
 import com.devbraga.bragacommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -20,5 +20,10 @@ public class ProductController {
     @GetMapping("/{id}")
     public ProductDTO findById(@PathVariable Long id){
         return productService.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return productService.findAll(pageable);
     }
 }
