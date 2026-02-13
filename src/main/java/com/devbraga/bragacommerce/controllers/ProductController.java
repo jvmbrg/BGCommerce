f8bd9fdff9719f2183a902270df052f218690731
jvmbrg <jvitormbraga@gmail.com>
2026-02-12 22:24:02 -0300

    Access control by role, by route

--- src/main/java/com/devbraga/bragacommerce/controllers/ProductController.java
+++ src/main/java/com/devbraga/bragacommerce/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
@@ -18,17 +19,19 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
-        return ResponseEntity.ok(productService.findById(id));
-    }
-
     @GetMapping
     public ResponseEntity<Page<ProductDTO>> findAll(@RequestParam(name = "name", defaultValue = "") String name,
                                                     Pageable pageable){
         return ResponseEntity.ok(productService.findAll(name, pageable));
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(productService.findById(id));
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/add")
     public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
 
