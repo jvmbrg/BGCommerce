4fc1102d564958227c150af272d14386ccf9eeb6
jvmbrg <jvitormbraga@gmail.com>
2026-02-24 10:42:51 -0300

    Test - Fix erros deploy

--- src/main/java/com/devbraga/bragacommerce/BgcommerceApplication.java
+++ src/main/java/com/devbraga/bragacommerce/BgcommerceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BgcommerceApplication {
-
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(BgcommerceApplication.class, args);
 	}
